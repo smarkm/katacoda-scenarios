ef5cc0f92eb36f0b9e0d43c93f4759b267cf539f
Smark <smark@freecoop.net>
2019-06-20 22:46:20 +0800

    update

--- snmptool/courseBase.sh
+++ snmptool/courseBase.sh
@@ -3,4 +3,5 @@ apt-get install -y snmpd snmp &&
 curl -LO https://github.com/smarkm/snmptool/releases/download/pre-0.0.1/snmptool &&
 chmod 755 snmptool &&
 mv snmptool /usr/bin/ &&
-echo =====env ok ======
+service snmpd start &&
+service snmpd status
--- snmptool/startSnmpd.sh
+++ snmptool/startSnmpd.sh
@@ -1,2 +1,1 @@
-service snmpd start
-service snmpd status
+snmptool version
