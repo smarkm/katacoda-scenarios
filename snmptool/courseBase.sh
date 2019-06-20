9460861878e8cfdc65b8caebb4c606e6c11b6fb1
Smark <smark@freecoop.net>
2019-06-20 22:39:57 +0800

    update

--- snmptool/courseBase.sh
+++ snmptool/courseBase.sh
@@ -1,4 +1,6 @@
 apt-get update && 
 apt-get install -y snmpd snmp &&
-echo snmpd already install successfully &&
-wget https://github.com/smarkm/snmptool/releases/download/pre-0.0.1/snmptool
+wget -LO https://github.com/smarkm/snmptool/releases/download/pre-0.0.1/snmptool &&
+chmod 755 snmptool &&
+mv snmptool /usr/bin/ &&
+echo =====env ok ======
--- snmptool/step2.md
+++ snmptool/step2.md
@@ -3,5 +3,6 @@ This is your first step.
 ## Task
 
 This is a learning env for snmptool
+* `snmptool version`
 * `snmptool sys`
 * `snmptool sys 127.0.0.1`
