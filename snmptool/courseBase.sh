794c7b5346bbcaf2203de0491ae64bdbfc2a03fb
Smark <smark@freecoop.net>
2020-02-16 07:45:06 +0800

    update to version v0.0.2
    Signed-off-by: Smark <smark@freecoop.net>

--- snmptool/courseBase.sh
+++ snmptool/courseBase.sh
@@ -1,6 +1,6 @@
 apt-get update && 
 apt-get install -y snmpd snmp &&
-curl -LO https://github.com/smarkm/snmptool/releases/download/pre-0.0.1/snmptool &&
+curl -LO https://github.com/smarkm/snmptool/releases/download/v0.0.2/snmptool &&
 chmod 755 snmptool &&
 mv snmptool /usr/bin/ &&
 service snmpd start &&
